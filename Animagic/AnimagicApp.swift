2ab7272c901b889eefdbc01e5776c36544b55bcb
dimaswisodewo <mdimaswisodewo26@cjkt.idserve.net>
2026-07-14 13:14:09 +0700

    Initial Commit

---
+++ Animagic/AnimagicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimagicApp.swift
+//  Animagic
+//
+//  Created by Meynabel Dimas Wisodewo on 14/07/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimagicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
